bd3d04d58ea99240ddeed25bf3bf45bfd7312c32
Deli Soetiawan <deli.soetiawan@gmail.com>
2022-02-08 16:09:28 +0700

    feat: add download link for story

--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -1,5 +1,6 @@
 <template>
   <Stories
+    @seeMore="open"
     :stories="items"
     :loop="true"
     :interval="5000"
@@ -12,16 +13,42 @@
 
 <script>
 import { Stories } from "vue-insta-stories";
-import StoryHeader from "./StoryHeader"
+import { saveAs } from "file-saver";
+import StoryHeader from "./StoryHeader";
+
 export default {
   components: { Stories, StoryHeader },
   data: () => ({
     items: [
-      require("../assets/1.png"),
-      require("../assets/2.png"),
-      require("../assets/3.png"),
-      require("../assets/4.png"),
+      {
+        url: require("../assets/1.png"),
+        type: 'image',
+        seeMore: { label: 'Download', url: require("../assets/1.png") },
+      },
+      {
+        url: require("../assets/2.png"),
+        type: 'image',
+        seeMore: { label: 'Download', url: require("../assets/2.png") },
+      },
+      {
+        url: require("../assets/3.png"),
+        type: 'image',
+        seeMore: { label: 'Download', url: require("../assets/3.png") },
+      },
+      {
+        url: require("../assets/4.png"),
+        type: 'image',
+        seeMore: { label: 'Download', url: require("../assets/4.png") },
+      },
     ]
-  })
+  }),
+  methods: {
+    open(story) {
+      const { url } = story.seeMore;
+      const filename = url.split('/');
+
+      saveAs(url, filename[filename.length - 1]);
+    }
+  }
 };
 </script>
